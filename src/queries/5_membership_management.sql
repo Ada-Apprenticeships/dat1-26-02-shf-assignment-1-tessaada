67f76b938889537cba13852ae6a9a1292f69f080
Tessa <237751177+tessaada@users.noreply.github.com>
2026-02-23 13:05:04 +0000

    Completed 5.1 and 5.3

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -1,16 +1,19 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 5.1 - DONE
-SELECT members.member_id, first_name, last_name, type, join_date
-    FROM members
-    JOIN memberships
-    ON members.member_id = memberships.member_id
-    WHERE memberships.status = 'Active';
-
+SELECT m.member_id, m.first_name, m.last_name, ms.type AS membership_type, m.join_date
+    FROM members AS m
+    JOIN memberships AS ms
+    ON m.member_id = ms.member_id
+    WHERE ms.status = 'Active';
 
 -- 5.2 
 
 
--- 5.3 
-
+-- 5.3 - DONE
+SELECT m.member_id, m.first_name, m.last_name, m.email, ms.end_date
+    FROM members AS m
+    JOIN memberships AS ms
+    ON m.member_id = ms.member_id
+    WHERE strftime('%Y', ms.end_date) = '2025';
